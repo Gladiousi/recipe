0a78d567e771d1414de4332d678d0cbb6f148d8a
gladiousi <abramovdaniil426475@gmail.com>
2026-02-06 10:41:24 +0300

    fix

--- src/lib/api/axios.ts
+++ src/lib/api/axios.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-declare const VITE_API_URL: string;
-
-const API_URL = (VITE_API_URL as string) || 'http://localhost:8000/api';
+const API_URL =
+  import.meta.env.VITE_API_URL || 'http://localhost:8000/api';
 
 export const api = axios.create({
   baseURL: API_URL,
